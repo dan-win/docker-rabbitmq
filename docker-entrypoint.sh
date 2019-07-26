f791594b55aedaae5e85f195531a11ff5edd73e0
Дмитрий Зимоглядов <zimoglyadov.d@tvzavr.ru>
2019-07-26 14:18:28 +0300

    fixed issue with zombies

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,11 @@
 set -eu
 
 echo "1 ============================================================================================================================="
-sudo rabbitmq-plugins enable rabbitmq_management 
+sudo rabbitmq-plugins enable rabbitmq_management
 echo "2 ============================================================================================================================="
 
 sudo service rabbitmq-server stop && \
-sudo service rabbitmq-server start 
+sudo service rabbitmq-server start
 
 echo "3 ============================================================================================================================="
 
@@ -17,6 +17,6 @@ echo "4 ========================================================================
 # Stop rabbit because main CMD in Dockerfile will start it direcly in order to see logs and output 
 sudo service rabbitmq-server stop
 
-# rabbitmq-server
+#exec rabbitmq-server
 
 exec "$@"
